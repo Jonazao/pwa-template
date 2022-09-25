1c4b6450a43e60e9d527b988677722cfb6fe14db
Jhonnatan Guerrero <Jhonnatan.Guerrero@foundersworkshop.com>
2022-09-25 01:15:44 -0700

    feat(init): Init PWA

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import useAddToHomeScreenPrompt from './hooks/useAddToHomeScreenPrompt';
 
 function App() {
+  const { isReady, promptToInstall, hasFinishedInstallation } = useAddToHomeScreenPrompt();
+  console.log(isReady, hasFinishedInstallation);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>💛 Muy Deli</h1>
+        <p>Instala esta aplicación en tu dispositivo</p>
+        <p>{isReady && !hasFinishedInstallation && <button onClick={() => promptToInstall()}>Instalar</button>}</p>
+        <p>{hasFinishedInstallation ? 'Estás usando la Web App' : 'Aplicación todavía no instalada'}</p>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register({
+  onUpdate: async (registration) => {
+    // We want to run this code only if we detect a new service worker is
+    // waiting to be activated.
+    // Details about it: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      // Makes Workbox call skipWaiting()
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      // Once the service worker is unregistered, we can reload the page to let
+      // the browser download a fresh copy of our app (invalidating the cache)
+      window.location.reload();
+    }
+  },
+});
