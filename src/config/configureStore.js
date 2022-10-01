15169e70bcc40aef314fc3a49f57b23a899af9b6
Jhonnatan Guerrero <Jhonnatan.Guerrero@foundersworkshop.com>
2022-10-01 03:13:32 -0700

    init(redux): add redux

--- src/config/configureStore.js
+++ src/config/configureStore.js
@@ -1,5 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { useAuthServer } from '../config/configureTemplate';
 
+import authReducer from '../features/auth/authSlice';
+
+const auth = useAuthServer ? authReducer : null;
+
+// This will throw a runtime error if no other reducer is present
 export const store = configureStore({
-  reducer: {},
+  reducer: { auth },
 });
---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isAuthenticated: null,
+  userId: null,
+  roles: null,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setAuth: (state, action) => {
+      state.auth = action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { setAuth } = authSlice.actions;
+
+export default authSlice.reducer;
